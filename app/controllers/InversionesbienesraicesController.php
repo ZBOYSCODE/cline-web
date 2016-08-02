56802a4e211cc9bde50cab3568409603e98d7088
Orlando San Martín <orlando.sanmartin@zentagroup.com>
2016-08-02 13:25:19 -0400

    fix rutas servidor

---
+++ app/controllers/InversionesbienesraicesController.php
@@ -0,0 +1,32 @@
+<?php
+
+class InversionesbienesraicesController extends \Phalcon\Mvc\Controller
+{
+
+    public function indexAction()
+    {
+       
+    }
+
+    public function casasAction(){
+        $this->view->pick("content/InversionesBienesRaices/casas");
+    }
+
+    public function departamentosAction(){
+        $this->view->pick("content/InversionesBienesRaices/departamentos");
+    }
+
+    public function localesComercialesAction(){
+        $this->view->pick("content/InversionesBienesRaices/localesComerciales");
+    }
+
+    public function segundaViviendaAction(){
+        $this->view->pick("content/InversionesBienesRaices/segundaVivienda");
+    }
+
+    public function vendoPropiedadAction(){
+        $this->view->pick("content/InversionesBienesRaices/vendoPropiedad");
+    }
+
+}
+
