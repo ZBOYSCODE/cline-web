dd122ad1dd37de27be2d1626901009fafb85c40b
miguelo <miguel.jara@zentagroup.com>
2016-11-03 08:04:19 -0300

    credline-web session radir

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -3,6 +3,8 @@
 use Phalcon\Mvc\Controller;
 use Phalcon\Mvc\Dispatcher;
 use \UserBicorp as UserBicorp;
+use Phalcon\Crypt;
+
 class ControllerBase extends Controller
 {
 	var $identity;
@@ -18,10 +20,13 @@ class ControllerBase extends Controller
 			$response = new \Phalcon\Http\Response();
 			if($user->profilesId == 'telecheque'){
 				$redirect = "http://telecheque.credline.cl";
-			}			
+			}
+			$key = "miguelos52675267";
+			$hash['id']=$user->id;
+			$hash['tk']=time();
+			$hashId = urlencode($crypt->encryptBase64(json_encode($hash), $key));
 			
-			
-			$response->redirect($redirect);
+			$response->redirect($redirect."/credline/login?id=".$hashId);
 			$response->send();
 		}		
     }
